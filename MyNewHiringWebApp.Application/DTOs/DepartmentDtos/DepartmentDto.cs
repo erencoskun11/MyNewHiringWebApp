af195b68c3d4c20380b069554b0b6f6a5fabe49d
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-08-22 18:10:07 +0300

    Add DepartmentDto

---
+++ MyNewHiringWebApp.Application/DTOs/DepartmentDtos/DepartmentDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewHiringWebApp.Application.DTOs.DepartmentDtos
+{
+    public class DepartmentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public List<JobPositionDto> Positions { get; set; } = new();
+    }
+}
