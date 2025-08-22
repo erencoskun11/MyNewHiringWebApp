f7c596bd90394dcce013d114c837d96cf28fee7a
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-08-22 18:07:28 +0300

    Add CandidateSkillUpdateDto

---
+++ MyNewHiringWebApp.Application/DTOs/CandidateSkillDtos/CandidateSkillUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewHiringWebApp.Application.DTOs.CandidateSkillDtos
+{
+    internal class CandidateSkillUpdateDto
+    {
+        public int Level { get; set; } // between 1 and 5 
+
+    }
+}
