7be7a39c1494a6ccc5fbec14a605425a3dfaac51
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-08-23 14:30:34 +0300

    Add CandidateRepository.cs

---
+++ MyNewHiringWebApp.Infrastructure/Repositories/CandidateRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyNewHiringWebApp.Domain.Entities;
+using MyNewHiringWebApp.Infrastructure.Data;
+using MyNewHiringWebApp.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewHiringWebApp.Infrastructure.Repositories
+{
+    internal class CandidateRepository : BaseRepository<Candidate>, ICandidateRepository
+    {
+        public CandidateRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+
+        public async Task<Candidate?> GetByEmailAsync(string email, CancellationToken ct = default)
+        {
+            return await _dbSet.FirstOrDefaultAsync(c => c.Email == email, ct);
+
+        }
+
+        public async Task<Candidate?> GetWithSkillsAsync(int id, CancellationToken ct = default)
+        {
+            return await _dbSet.Include(c => c.CandidateSkills).FirstOrDefaultAsync(c=>c.Id == id, ct);
+        }
+    }
+}
