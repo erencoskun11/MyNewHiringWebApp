293e4c677b6fa6b22a5315312cec1b458ece5321
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-08-23 16:20:40 +0300

    fix(infra): update DepartmentRepository to implement new interface location

--- MyNewHiringWebApp.Infrastructure/Repositories/DepartmentRepository.cs
+++ MyNewHiringWebApp.Infrastructure/Repositories/DepartmentRepository.cs
@@ -1,13 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MyNewHiringWebApp.Application.Interface;
 using MyNewHiringWebApp.Domain.Entities;
 using MyNewHiringWebApp.Infrastructure.Data;
-using MyNewHiringWebApp.Infrastructure.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyNewHiringWebApp.Infrastructure.Repositories
 {
