5aa050afecf4b31900f3f57dd9139eec668cbee0
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-08-23 16:28:22 +0300

    refactor(application): move interfaces into Application layer

---
+++ MyNewHiringWebApp.Application/Interface/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewHiringWebApp.Application.Interface
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id, CancellationToken ct = default);
+        Task<T?> FindAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default);
+        Task<IReadOnlyList<T>> ListAsync(CancellationToken ct = default);
+        Task<IReadOnlyList<T>> ListAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default);
+
+        Task AddAsync(T entity, CancellationToken ct = default);
+        void Update(T entity);
+        void Remove(T entity);
+
+        // Pagination
+        Task<(IReadOnlyList<T> Items, int TotalCount)> GetPagedAsync(
+            int page, int pageSize, Expression<Func<T, bool>>? predicate = null, CancellationToken ct = default);
+    }
+}
