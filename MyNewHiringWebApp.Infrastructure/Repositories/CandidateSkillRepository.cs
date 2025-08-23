d346f4c128668c76814f71dda731c7314dac0612
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-08-23 14:30:34 +0300

    Add CandidateSkillRepository.cs

---
+++ MyNewHiringWebApp.Infrastructure/Repositories/CandidateSkillRepository.cs
@@ -0,0 +1,17 @@
+﻿using MyNewHiringWebApp.Infrastructure.Data;
+using MyNewHiringWebApp.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewHiringWebApp.Infrastructure.Repositories
+{
+    public class CandidateSkillRepository : BaseRepository<CandidateSkill>, ICandidateSkillRepository
+    {
+        public CandidateSkillRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+    }
+}
