d41bf5c787a8e2f1e94f7ec4676c05085afb13e6
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-08-23 14:30:35 +0300

    Add InterviewRepository.cs

---
+++ MyNewHiringWebApp.Infrastructure/Repositories/InterviewRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyNewHiringWebApp.Domain.Entities;
+using MyNewHiringWebApp.Infrastructure.Data;
+using MyNewHiringWebApp.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewHiringWebApp.Infrastructure.Repositories
+{
+    public class InterviewRepository : BaseRepository<Interview> ,IInterviewRepository
+    {
+        public InterviewRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+
+        public async Task<Interview?> GetWithRelationsAsync(int id, CancellationToken ct = default)
+        {
+            return await _dbSet
+               .Include(i => i.JobApplication)         
+               .Include(i => i.Interviewer)        
+               .FirstOrDefaultAsync(i => i.Id == id, ct);
+        }
+    }
+}
