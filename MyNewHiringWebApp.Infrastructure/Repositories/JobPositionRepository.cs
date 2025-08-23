da2239efcc706835d07849d0813c1a52d03603fb
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-08-23 16:20:43 +0300

    fix(infra): update JobPositionRepository to Application interface

--- MyNewHiringWebApp.Infrastructure/Repositories/JobPositionRepository.cs
+++ MyNewHiringWebApp.Infrastructure/Repositories/JobPositionRepository.cs
@@ -1,11 +1,6 @@
-﻿using MyNewHiringWebApp.Domain.Entities;
+﻿using MyNewHiringWebApp.Application.Interface;
+using MyNewHiringWebApp.Domain.Entities;
 using MyNewHiringWebApp.Infrastructure.Data;
-using MyNewHiringWebApp.Infrastructure.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyNewHiringWebApp.Infrastructure.Repositories
 {
