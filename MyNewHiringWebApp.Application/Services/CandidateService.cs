3290d87ffb0aa1e4301328586ebbc836697de956
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-08-26 17:07:47 +0300

    Apply caching attributes to CandidateService methods

--- MyNewHiringWebApp.Application/Services/CandidateService.cs
+++ MyNewHiringWebApp.Application/Services/CandidateService.cs
@@ -2,6 +2,7 @@
 using MyNewHiringWebApp.Application.DTOs.CandidateDtos;
 using MyNewHiringWebApp.Application.Interface;
 using MyNewHiringWebApp.Application.InterfaceServices;
+using MyNewHiringWebApp.Application.Services.Caching;
 using MyNewHiringWebApp.Domain.Entities;
 using System;
 using System.Collections.Generic;
@@ -18,9 +19,16 @@ namespace MyNewHiringWebApp.Application.Services
         CandidateUpdateDto    
     >, ICandidateService     
     {
-        public CandidateService(IRepository<Candidate> repo, IMapper mapper)
+        private readonly ICacheService _cache;
+        private string GetAllKey => "candidates:all";
+        private string GetByEmailKey(string email) => $"candidates:email:{email}";
+        private string GetBySkillKey(int skillId) => $"candidates:skill:{skillId}";
+        private string SearchByNameKey(string name) => $"candidates:search:{name}";
+
+        public CandidateService(IRepository<Candidate> repo, IMapper mapper,ICacheService cache)
             : base(repo, mapper)
         {
+            _cache = cache;
         }
         public async Task<IEnumerable<CandidateDto>> GetAllByEmailAsync(string email, CancellationToken ct = default)
         {
@@ -36,25 +44,59 @@ namespace MyNewHiringWebApp.Application.Services
 
         public async Task<CandidateDto?> GetByEmailAsync(string email, CancellationToken ct = default)
         {
+            var cacheKey = GetByEmailKey(email);
+
+            //1.Cache control
+            var cached = await _cache.GetAsync<CandidateDto>(cacheKey);
+            if (cached != null) return cached;
+
+            //2. pull from db
+
             var candidate = await _repo.FindAsync(c => c.Email == email, ct);
-            return candidate == null ? null : _mapper.Map<CandidateDto>(candidate);
+            if (candidate == null) return null;
+
+            var dto = _mapper.Map<CandidateDto>(candidate);
+
+            await _cache.SetAsync(cacheKey, dto, TimeSpan.FromMinutes(5));
+            return dto;
+        
+        
         }
 
         public async Task<IEnumerable<CandidateDto>> GetBySkillAsync(int skillId, CancellationToken ct = default)
         {
+            var cacheKey = GetBySkillKey(skillId);
+
+            var cached = await _cache.GetAsync<IEnumerable<CandidateDto>>(cacheKey);
+            if (cached != null) return cached;
+
+
             var candidates = await _repo.ListAsync(c => c.CandidateSkills.Any(cs => cs.SkillId == skillId), ct);
-            return _mapper.Map<IEnumerable<CandidateDto>>(candidates);
+            var result = _mapper.Map<IEnumerable<CandidateDto>>(candidates);
+
+            await _cache.SetAsync(cacheKey, result, TimeSpan.FromMinutes(10));
+
+            return result;
+        
         }
 
         public async Task<IEnumerable<CandidateDto>> SearchByNameAsync(string name, CancellationToken ct = default)
         {
+            var cacheKey = SearchByNameKey(name);
+
+            var cached = await _cache.GetAsync<IEnumerable<CandidateDto>>(cacheKey);
+            if(cached != null) return cached;
+
+
             var candidates = await _repo.ListAsync(c =>
            c.FirstName.ToLower().Contains(name.ToLower()) ||
             c.LastName.ToLower().Contains(name.ToLower()),ct);
 
+            var result = _mapper.Map<IEnumerable<CandidateDto>>(candidates);
 
-            return _mapper.Map<IEnumerable<CandidateDto>>(candidates);
-
+            await _cache.SetAsync(cacheKey,result, TimeSpan.FromMinutes(10));
+            
+            return result;
             
         }
     }
