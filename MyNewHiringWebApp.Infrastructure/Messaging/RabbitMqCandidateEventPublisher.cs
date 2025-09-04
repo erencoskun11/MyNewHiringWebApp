89297393576ffd6d4836e4d3a62f5af5ec6e1934
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-04 12:05:31 +0300

    feat(rabbitmq): add RabbitMqCandidateEventPublisher class

---
+++ MyNewHiringWebApp.Infrastructure/Messaging/RabbitMqCandidateEventPublisher.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using MyNewHiringWebApp.Application.Messaging.Events;
+using MyNewHiringWebApp.Application.Messaging.Interfaces;
+using RabbitMQ.Client;
+namespace MyNewHiringWebApp.Infrastructure.Messaging
+{
+    public class RabbitMqCandidateEventPublisher : ICandidateEventPublisher
+    {
+        private readonly RabbitMqConnectionFactory _connectionFactory;
+        private const string QueueName = "candidate.created.queue";
+
+
+        public RabbitMqCandidateEventPublisher(RabbitMqConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public Task PublishCandidateCreatedAsync(CandidateCreatedEvent @event)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            
+            channel.QueueDeclare(queue: QueueName,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var body : Encoding.UTF8.GetBytes(JsonSerializer.Serialize(@event));
+
+            var props = channel.CreateBasicProperties();
+            props.Deli
+            channel.BasicPublish(exchange: "",
+                rotingKey: _queueName,
+                basicProperties: props,
+                body: body);
+
+            return Task.ComletedTask;     
+        }
+    }
+}
