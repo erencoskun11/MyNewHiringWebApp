7fb8fd8378f92a69479a8d286d2a93e24f3dfe55
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-04 12:08:24 +0300

    rabbitMq consigurations

--- MyNewHiringWebApp.Application/Mappings/MappingProfile.cs
+++ MyNewHiringWebApp.Application/Mappings/MappingProfile.cs
@@ -12,6 +12,7 @@ using MyNewHiringWebApp.Application.DTOs.SubmittedAnswerCreateDtos;
 using MyNewHiringWebApp.Application.DTOs.TestDtos;
 using MyNewHiringWebApp.Application.DTOs.TestQuestionDtos;
 using MyNewHiringWebApp.Application.Eto;
+using MyNewHiringWebApp.Application.ETOs.CandidateEtos;
 using MyNewHiringWebApp.Domain.Entities;
 using System.Linq;
 
@@ -22,18 +23,16 @@ namespace MyNewHiringWebApp.Application.Mappings
         public MappingProfile()
         {
             // ---------- Candidate ----------
-            // Create / Update DTO -> Entity
             CreateMap<CandidateCreateDto, Candidate>()
                 .ForMember(dest => dest.Resume, opt => opt.MapFrom(src =>
                     src == null ? null : new ResumeSummary(src.ResumeSummary, src.GithubUrl, src.LinkedInUrl)))
-                .ForMember(dest => dest.CandidateSkills, opt => opt.Ignore()); // skill'leri servisle ekle
+                .ForMember(dest => dest.CandidateSkills, opt => opt.Ignore());
 
             CreateMap<CandidateUpdateDto, Candidate>()
                 .ForMember(dest => dest.Resume, opt => opt.MapFrom(src =>
                     src == null ? null : new ResumeSummary(src.ResumeSummary, src.GithubUrl, src.LinkedInUrl)))
                 .ForMember(dest => dest.CandidateSkills, opt => opt.Ignore());
 
-            // Entity -> DTO (read)
             CreateMap<Candidate, CandidateDto>()
                 .ForMember(dest => dest.ResumeSummary, opt => opt.MapFrom(src => src.Resume != null ? src.Resume.Summary : null))
                 .ForMember(dest => dest.GithubUrl, opt => opt.MapFrom(src => src.Resume != null ? src.Resume.GithubUrl : null))
@@ -42,48 +41,32 @@ namespace MyNewHiringWebApp.Application.Mappings
                     opt => opt.MapFrom(src =>
                         src.CandidateSkills == null
                             ? null
-                            : src.CandidateSkills.Select(cs => new MyNewHiringWebApp.Application.DTOs.CandidateDtos.CandidateSkillDto
+                            : src.CandidateSkills.Select(cs => new CandidateSkillDto
                             {
                                 SkillName = cs.Skill != null ? cs.Skill.Name : string.Empty,
                                 Level = cs.Level
                             }).ToList()
                     ));
 
-            // ---------- CandidateSkill ----------
-            // If you have separate CandidateSkill DTOs, map them explicitly:
             CreateMap<CandidateSkillCreateDto, CandidateSkill>().ReverseMap();
             CreateMap<CandidateSkillUpdateDto, CandidateSkill>().ReverseMap();
 
-            // Map domain CandidateSkill -> CandidateSkillDto (from candidate-dedicated DTO namespace)
-            // Use fully-qualified target type to avoid ambiguity between namespaces
-            CreateMap<CandidateSkill, MyNewHiringWebApp.Application.DTOs.CandidateSkillDtos.CandidateSkillDto>()
+            CreateMap<CandidateSkill, CandidateSkillDto>()
                 .ForMember(d => d.SkillName, opt => opt.MapFrom(s => s.Skill != null ? s.Skill.Name : string.Empty))
                 .ForMember(d => d.Level, opt => opt.MapFrom(s => s.Level));
 
-            // Also map CandidateSkill -> CandidateDto's inner CandidateSkillDto (if used)
-            CreateMap<CandidateSkill, MyNewHiringWebApp.Application.DTOs.CandidateDtos.CandidateSkillDto>()
-                .ForMember(d => d.SkillName, opt => opt.MapFrom(s => s.Skill != null ? s.Skill.Name : string.Empty))
-                .ForMember(d => d.Level, opt => opt.MapFrom(s => s.Level));
-
-
-            //rabbit icin eto 
+            // Candidate -> ETO for RabbitMQ
             CreateMap<Candidate, CandidateCreatedEto>()
                 .ForMember(dest => dest.CandidateId, opt => opt.MapFrom(src => src.Id));
-        }
 
-
-
-
-
-        // ---------- Department ----------
-        CreateMap<DepartmentCreateDto, Department>().ReverseMap();
+            // ---------- Department ----------
+            CreateMap<DepartmentCreateDto, Department>().ReverseMap();
             CreateMap<DepartmentUpdateDto, Department>().ReverseMap();
             CreateMap<Department, DepartmentDto>().ReverseMap();
 
             // ---------- Interview ----------
             CreateMap<InterviewCreateDto, Interview>().ReverseMap();
             CreateMap<InterviewUpdateDto, Interview>().ReverseMap();
-
             CreateMap<Interview, InterviewDto>()
                 .ForMember(d => d.JobApplicationTitle, opt => opt.MapFrom(src => src.JobApplication != null ? src.JobApplication.JobPosition.Title : null))
                 .ForMember(d => d.InterviewerName, opt => opt.MapFrom(src => src.Interviewer != null ? src.Interviewer.FullName : null));
@@ -91,14 +74,12 @@ namespace MyNewHiringWebApp.Application.Mappings
             // ---------- Interviewer ----------
             CreateMap<InterviewerCreateDto, Interviewer>().ReverseMap();
             CreateMap<InterviewerUpdateDto, Interviewer>().ReverseMap();
-
             CreateMap<Interviewer, InterviewerDto>()
                 .ForMember(d => d.InterviewCount, opt => opt.MapFrom(src => src.Interviews != null ? src.Interviews.Count : 0));
 
             // ---------- JobApplication ----------
             CreateMap<JobApplicationCreateDto, JobApplication>().ReverseMap();
             CreateMap<JobApplicationUpdateDto, JobApplication>().ReverseMap();
-
             CreateMap<JobApplication, JobApplicationDto>()
                 .ForMember(dest => dest.CandidateName,
                     opt => opt.MapFrom(src => src.Candidate != null ? src.Candidate.FirstName + " " + src.Candidate.LastName : null))
@@ -109,7 +90,6 @@ namespace MyNewHiringWebApp.Application.Mappings
             CreateMap<JobPositionCreateDto, JobPosition>().ReverseMap();
             CreateMap<JobPositionUpdateDto, JobPosition>()
                 .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
-
             CreateMap<JobPosition, JobPositionDto>()
                 .ForMember(dest => dest.DepartmentName, opt => opt.MapFrom(src => src.Department != null ? src.Department.Name : null))
                 .ReverseMap();
@@ -117,7 +97,6 @@ namespace MyNewHiringWebApp.Application.Mappings
             // ---------- Skill ----------
             CreateMap<SkillCreateDto, Skill>().ReverseMap();
             CreateMap<SkillUpdateDto, Skill>().ReverseMap();
-
             CreateMap<Skill, SkillDto>()
                 .ForMember(dest => dest.CandidateCount, opt => opt.MapFrom(src => src.CandidateSkills != null ? src.CandidateSkills.Count : 0))
                 .ReverseMap();
