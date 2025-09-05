e44c2cf2d3e1a85c67d6122ab7d1ea65fab78104
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-05 11:55:48 +0300

    Created ICandidateSkillEventPublisher

---
+++ MyNewHiringWebApp.Application/Messaging/Interfaces/ICandidateSkillEventPublisher.cs
@@ -0,0 +1,14 @@
+﻿using MyNewHiringWebApp.Application.ETOs.CandidateSkillsEtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewHiringWebApp.Application.Messaging.Interfaces
+{
+    public interface ICandidateSkillEventPublisher
+    {
+        Task PublisherCandidateSkillCreatedAsync(CandidateSkillCreatedEto eto);
+    }
+}
